55fd2f935dfa6992c199c84a4cfbb7e168074d22
Christopher Maye <mayec15@students.ecu.edu>
2020-11-10 18:26:36 -0500

    attempt to fix the underrun bug

--- SceneGame.py
+++ SceneGame.py
@@ -38,9 +38,6 @@ class SceneGame(SceneBasic):
 	STATE_NORMAL = 0
 	STATE_SOLVED = 1
 
-	# Number of points given each time a correct answer is given.
-	# Value is modified by accuracy for the question
-	SCORE_AWARDED = 10
 
 	def __init__(self,screenSize):
 		SceneBasic.__init__(self,screenSize)
@@ -238,7 +235,7 @@ class SceneGame(SceneBasic):
 		print("LOADING NEW QUESTION ")
 		self.questionChoices	= choices
 		self.questionAnswers	= answers
-		SCORE_AWARDED = 10
+		self.award = 10
 		for i in range(0,3):
 			self.arrIcnFuels[i].setSelect(False)
 			self.arrIcnFuels[i].display(choices[i][0],choices[i][1])
@@ -290,10 +287,10 @@ class SceneGame(SceneBasic):
 			self.icnRocket.launch()
 			#Submitted answer is correct advnace to the next level and raise win event
 			self.questionLevel += 1
-			self.score += SCORE_AWARDED
+			self.score += self.award
 		else : 
-			if SCORE_AWARDED != 1:
-				SCORE_AWARDED -= 3
+			if self.award != 1:
+				self.award -= 3
 			if self.score != 0:
 				self.score -= 1
 			SoundManager.ANSWER_WRONG()
@@ -321,6 +318,7 @@ class SceneGame(SceneBasic):
 		self.icnRocket.reset()
 		self.questionLevel = 0 
 		self.score = 0
+		self.award = 10
 		self.icnTextScore.setContent("Score 0")
 		self.icnTextLevel.setContent("Level 0")
 		pass
